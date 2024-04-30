20fd9b737d33390824e70e2c55370403ab5ecbd3
SamuelErazo <Samuel.erazo.m@uniautonoma.edu.co>
2024-04-30 18:54:45 -0500

    first commit

---
+++ resources/views/bienvenido.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('titulo', 'sobre desastres')
+@section('contenido')
+    <p>Natural disasters are catastrophic events that occur as a result of natural processes of the Earth. These events
+        can cause significant damage to property, infrastructure, and the environment, as well as loss of life and
+        displacement of communities. Some examples of natural disasters include earthquakes, hurricanes, floods,
+        tsunamis, tornadoes, volcanic eruptions, and wildfires.</p>
+    <p>Earthquakes are caused by the sudden release of energy in the Earth's crust, resulting in shaking of the ground.
+        They can cause buildings and infrastructure to collapse, leading to casualties and destruction.</p>
+    <p>Hurricanes, also known as cyclones or typhoons depending on the region, are powerful tropical storms with strong
+        winds and heavy rainfall. They can cause extensive flooding, wind damage, and storm surges along coastal areas.
+    </p>
+    <p>Floods occur when water overflows onto land that is usually dry. They can result from heavy rainfall, rapid
+        snowmelt, or the failure of dams and levees. Floods can cause widespread damage to property and infrastructure,
+        as well as loss of life.</p>
+@endsection
---
+++ resources/views/earthquake.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('titulo', 'terremoto')
+@section('contenido')
+    <h1>Terremoto</h1>
+    <p>bla bla bla</p>
+
+@endsection
